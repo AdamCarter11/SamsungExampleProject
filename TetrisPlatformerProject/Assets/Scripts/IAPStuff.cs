8c51aaf84e731dd8799adce40da7953d2f976228
Adam <57573881+AdamCarter11@users.noreply.github.com>
2022-02-18 14:06:50 -0800

    added purchase buttons

--- TetrisPlatformerProject/Assets/Scripts/IAPStuff.cs
+++ TetrisPlatformerProject/Assets/Scripts/IAPStuff.cs
@@ -8,7 +8,7 @@ public class IAPStuff : MonoBehaviour
 {
     //private System.Action<Samsung.ProductInfoList> lst;
     //private List<Samsung.ProductVo> listOfProducts;
-    [SerializeField] private Button productsButton;
+    [SerializeField] private Button productsButton, itemOneButton;
     [SerializeField] private Dropdown itemList;
     [SerializeField] private Text itemText;
     void Start()
@@ -17,7 +17,8 @@ public class IAPStuff : MonoBehaviour
         //Samsung.SamsungIAP.Instance.GetProductsDetails("", lst);
         //test when seller account goes through
         //Samsung.SamsungIAP.Instance.SetOperationMode(Samsung.OperationMode.OPERATION_MODE_TEST_FAILURE);
-        productsButton.onClick.AddListener(OnGetProductsButton);
+        productsButton.onClick.AddListener(OnGetProductsButton);        
+        productsButton.onClick.AddListener(OnBuyTestItem);
     }
 
     void Update()
@@ -25,6 +26,104 @@ public class IAPStuff : MonoBehaviour
 
     }
     
+    //gets the products (not necessarily owned)
+    void OnGetProductsDetails(ProductInfoList _productList){
+        if (_productList.errorInfo != null){
+            if (_productList.errorInfo.errorCode == 0){// 0 means no error
+                if (_productList.results != null){
+                    itemList.ClearOptions();
+                    List<string> optionItems = new List<string>();
+                    int i = 1;
+                    foreach (ProductVo item in _productList.results){
+                            string temp = i+ ". " + item.mItemName + ": $ " + item.mItemPrice;
+                            optionItems.Add(temp);
+                            i++;
+                    }
+                    itemList.AddOptions(optionItems);
+                }
+            }
+        }
+    }
+
+    //used for consumables
+    void OnConsume(ConsumedList _consumedList){
+        if(_consumedList.errorInfo != null){
+            if(_consumedList.errorInfo.errorCode == 0){
+                if(_consumedList.results != null){
+                    foreach(ConsumeVo item in _consumedList.results){
+                        if(item.mStatusCode == 0){
+                            //successfully consumed and ready to be purchased again.
+                        }
+                 }
+             }
+         }
+     }
+    }
+
+    //Used to get what you already own
+    void OnGetOwnedList(OwnedProductList _ownedProductList){
+        if(_ownedProductList.errorInfo != null){
+            if(_ownedProductList.errorInfo.errorCode == 0){// 0 means no error
+                if(_ownedProductList.results != null){
+                    foreach(OwnedProductVo item in _ownedProductList.results){
+                        if(item.mConsumableYN == "Y"){
+                            //consume the consumable items and OnConsume callback is triggered afterwards
+                            SamsungIAP.Instance.ConsumePurchasedItems(item.mPurchaseId, OnConsume);
+                        }
+                        if(item.mItemId == "1"){
+                            //superJump++;
+                            itemText.text = "You bought first IAP";
+                        }
+                        else if(item.mItemId == "BuyUpgradedPlayer"){                         
+                            //playerMaterial = Resources.Load<Material>("playerMaterial");
+                            //MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+                            //meshRenderer.material = playerMaterial;                        
+                        }                    
+                    }
+                } 
+            }
+        }
+    }
+    
+    //function to trigger payment
+    void OnPayment(PurchasedInfo _purchaseInfo){
+        if(_purchaseInfo.errorInfo != null){
+            if(_purchaseInfo.errorInfo.errorCode == 0){
+                if(_purchaseInfo.results != null){
+                    //your purchase is successful
+                    if(_purchaseInfo.results.mConsumableYN == "Y"){
+                        //consume the consumable items
+                        SamsungIAP.Instance.ConsumePurchasedItems(_purchaseInfo.results.mPurchaseId, OnConsume);
+                    }
+                    if(_purchaseInfo.results.mItemId == "1"){
+                        itemText.text = "You bought first IAP";
+                    }
+                    /*
+                    else if(_purchaseInfo.results.mItemId == "BuyUpgradedPlayer"){
+                        playerMaterial = Resources.Load<Material>("playerMaterial");
+                        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+                        meshRenderer.material = playerMaterial;
+                    }
+                    */
+                }
+            }
+        }
+    }
+
+    //button functions (for triggering events)
+    void OnGetProductsButton(){
+        //get all the product details
+        SamsungIAP.Instance.GetProductsDetails("", OnGetProductsDetails); 
+    }  
+    void OnBuyTestItem(){
+        SamsungIAP.Instance.StartPayment("BuySuperJump", "", OnPayment);
+    }
+}
+
+
+
+
+ 
     /*
     void returnInfoList(Samsung.ProductInfoList pil){
         int i = 0;
@@ -49,63 +148,3 @@ public class IAPStuff : MonoBehaviour
 
     }
     */
-    void OnGetProductsButton(){
-        //get all the product details
-        SamsungIAP.Instance.GetProductsDetails("", OnGetProductsDetails); 
-    }     
-    void OnGetProductsDetails(ProductInfoList _productList){
-        if (_productList.errorInfo != null){
-            if (_productList.errorInfo.errorCode == 0){// 0 means no error
-                if (_productList.results != null){
-                    itemList.ClearOptions();
-                    List<string> optionItems = new List<string>();
-                    int i = 1;
-                    foreach (ProductVo item in _productList.results){
-                            string temp = i+ ". " + item.mItemName + ": $ " + item.mItemPrice;
-                            optionItems.Add(temp);
-                            i++;
-                    }
-                    itemList.AddOptions(optionItems);
-                }
-            }
-        }
-    }
-
-    void OnConsume(ConsumedList _consumedList){
-        if(_consumedList.errorInfo != null){
-            if(_consumedList.errorInfo.errorCode == 0){
-                if(_consumedList.results != null){
-                    foreach(ConsumeVo item in _consumedList.results){
-                        if(item.mStatusCode == 0){
-                            //successfully consumed and ready to be purchased again.
-                        }
-                 }
-             }
-         }
-     }
-    }
-
-    void OnGetOwnedList(OwnedProductList _ownedProductList){
-        if(_ownedProductList.errorInfo != null){
-            if(_ownedProductList.errorInfo.errorCode == 0){// 0 means no error
-                if(_ownedProductList.results != null){
-                    foreach(OwnedProductVo item in _ownedProductList.results){
-                        if(item.mConsumableYN == "Y"){
-                            //consume the consumable items and OnConsume callback is triggered afterwards
-                            SamsungIAP.Instance.ConsumePurchasedItems(item.mPurchaseId, OnConsume);
-                        }
-                        if(item.mItemId == "1"){
-                            //superJump++;
-                            itemText.text = "You bought first IAP";
-                        }
-                        else if(item.mItemId == "BuyUpgradedPlayer"){                         
-                            //playerMaterial = Resources.Load<Material>("playerMaterial");
-                            //MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
-                            //meshRenderer.material = playerMaterial;                        
-                        }                    
-                    }
-                } 
-            }
-        }
-    }
-}
